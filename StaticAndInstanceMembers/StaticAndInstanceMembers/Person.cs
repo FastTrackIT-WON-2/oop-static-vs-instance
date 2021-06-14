524cc29313f47ca3498041879ee82f7f38ed729a
Florin Lazar <florin.lazar.it2014@gmail.com>
2021-06-14 23:34:22 +0300

    Exercise with static - vs - instance members

---
+++ StaticAndInstanceMembers/StaticAndInstanceMembers/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticAndInstanceMembers
+{
+    public class Person
+    {
+        public string FirstName, LastName;
+
+        internal static int TotalPopulation;
+
+        public void Print()
+        {
+            Console.WriteLine($"{FirstName} {LastName} {TotalPopulation}");
+        }
+
+        public static void PrintTotalPopulation()
+        {
+            Console.WriteLine($"Total population is: {TotalPopulation}");
+        }
+
+        public static void PrintPerson(Person p)
+        {
+            Console.WriteLine($"Person: {p.FirstName} {p.LastName}");
+        }
+
+        public static int GetARandomNumber()
+        {
+            return 100034343;
+        }
+    }
+}
---
+++ StaticAndInstanceMembers/StaticAndInstanceMembers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StaticAndInstanceMembers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.FirstName = "John";
+            person1.LastName = "Doe";
+            Person.TotalPopulation = 1;
+
+            Person person2 = new Person();
+            person2.FirstName = "Mary";
+            person2.LastName = "Doe";
+            Person.TotalPopulation = 2;
+
+            person1.Print();
+            person2.Print();
+            Person.PrintTotalPopulation();
+
+            // Console.WriteLine($"Person 1: {person1.FirstName} {person1.LastName} {Person.TotalPopulation}");
+            // Console.WriteLine($"Person 2: {person2.FirstName} {person2.LastName} {Person.TotalPopulation}");
+        }
+    }
+}
